25af1b6232f6c98afd157b4b56ba8f8fbf9a2982
Your Name <you@example.com>
2022-03-10 23:17:54 +0800

    update

--- src/components/GoodsList.vue
+++ src/components/GoodsList.vue
@@ -1,24 +1,11 @@
 <template>
   <div class="goods-list">
-    <!-- <GoodsItem v-for="(item, key) in goodsList" :key="key" /> -->
-    <div class="container l-class">
-      <!-- <image bind:tap="onMore" class="title" src="{{theme.title_img}}"></image> -->
-      <div scroll-x class="scroll-view">
-        <div class="inner">
-          <!-- <block wx:for="{{spuList}}" wx:key="index"> -->
-          <div bind:tap="onTap" class="spu-container">
-            <!-- <image class="spu-img" src="{{item.img}}"></image> -->
-            <p class="goods-img"></p>
-            <span class="spu-text">title</span>
-            <span class="price-value price-unit">123</span>
-            <!-- <l-price
-                                l-value-class="price-value"
-                                l-unit-class="price-unit"
-                                value="{{p.mainPrice(item.price,item.discount_price).price}}">
-            </l-price>-->
-          </div>
-          <!-- </block> -->
-        </div>
+    <div class="goods-item">
+      <p class="goods-img"></p>
+      <div style="padding: 0.16rem">
+        <P class="goods-title">北欧简约立式台灯</P>
+        <P class="goods-pic">¥ 298</P>
+        <P class="goods-subtitle">简约北欧风，单色立式台灯搭配性冷淡风格</P>
       </div>
     </div>
   </div>
@@ -42,61 +29,38 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+.goods-list {
+  padding: 0.2rem 0.2rem 2rem;
+}
 .goods-img {
-  width: 2rem;
-  height: 2rem;
+  width: 100%;
+  height: 3rem;
   background-color: #ccc;
 }
 
-/* components/spu-scroll/index.wxss */
-.container {
-  box-sizing: border-box;
-  width: 100%;
-  padding: 0.42rem 0 0.34rem 0.28rem;
-  background-color: #ffffff;
-}
+.goods-item {
+  // border: 2px solid #ddd;
+  border-radius: 0.1rem;
+  box-shadow: 0px 0px 3px 0px rgba(221, 221, 221, 0.5);
 
-.title {
-  width: 0.694rem;
-  height: 0.9rem;
-}
+  width: 50%;
+  text-align: left;
 
-.price-unit {
-  font-size: 0.24rem !important;
-  color: #157658 !important;
+  background-color: #fff;
 }
-
-.price-value {
-  font-size: 0.26rem !important;
-  color: #157658 !important;
+.goods-title {
+  font-size: 0.28rem;
+  color: #333;
 }
-
-.scroll-view {
-  margin-top: 0.28rem;
-  display: flex;
-  flex-direction: row;
+.goods-pic {
+  padding: 0.1rem;
+  font-size: 0.32rem;
+  color: #157658;
+  font-weight: bold;
 }
-
-.inner {
-  display: flex;
-  flex-direction: row;
-}
-
-.spu-container {
-  margin-right: 0.6rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.spu-text {
-  font-size: 0.2rem;
-  color: #666666;
-  margin-top: 0.2rem;
-}
-
-.spu-img {
-  width: 0.16rem;
-  height: 0.16rem;
+.goods-subtitle {
+  font-size: 0.24rem;
+  color: #999;
+  line-height: 0.36rem;
 }
 </style>
