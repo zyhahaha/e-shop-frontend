1d7161323957966cf915e66dffb81aede3d2248a
Your Name <you@example.com>
2022-03-13 23:34:38 +0800

    update

--- xxxxxxxxxxxxxx/qualcomm/js/share.js
+++ xxxxxxxxxxxxxx/qualcomm/js/share.js
@@ -1,9 +1,18 @@
-var shareEl = document.getElementsByClassName('share')[0]
-function openShare(){
-    if (!shareEl) return false;
-    shareEl.style.top = '0rem'
-}
-function closeShare(){
-    if (!shareEl) return false;
-    shareEl.style.top = '100vh'
-}
+var _shareCtrl = (function() {
+    var shareEl = document.getElementsByClassName('share')[0]
+    shareEl.addEventListener('click', function() {
+        closeShare()
+    })
+    function openShare(){
+        if (!shareEl) return false;
+        shareEl.style.top = '0rem'
+    }
+    function closeShare(){
+        if (!shareEl) return false;
+        shareEl.style.top = '100vh'
+    }
+    return {
+        openShare,
+        closeShare
+    }
+})()
