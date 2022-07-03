338b2147c15fe9de960c648cd6b961dc7f5575a7
zyhahaha <zy980355088@gmail.com>
2022-07-03 11:24:50 +0800

    update

--- src/libs/constant.ts
+++ src/libs/constant.ts
@@ -1,1 +1,2 @@
-export const VUE_APP_IMAGE_HOST='http://rd78eetxk.hn-bkt.clouddn.com'
+// export const VUE_APP_IMAGE_HOST='http://rd78eetxk.hn-bkt.clouddn.com'
+export const VUE_APP_IMAGE_HOST='http://cdn.resource.123123.store'
